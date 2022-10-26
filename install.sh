2b2de04ca69bef61e8435f462f460044e0801e22
ccwpmigration <116763862+ccwpmigration@users.noreply.github.com>
2022-10-26 19:40:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # SCRIPT: install.sh
-# PURPOSE: Install cPanelIMAP IMAPSYNC UI for cPanel
+# PURPOSE: Install cPanelIMAP IMAPSYNC GUI for cPanel
 # AUTHOR: StephenA <support@ccwpmigration.xyz>
 #
-echo "Installing IMAP Sync UI - Paper Lantern"
+echo "Installing cPanelIMAP IMAPSYNC GUI for cPanel - Paper Lantern"
 mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/cc_imapsync
 cd /usr/local/cpanel/base/frontend/paper_lantern/cc_imapsync
 wget https://github.com/ccwpmigration/cPanelIMAP/raw/main/cc_imapsync.tar.gz
@@ -12,10 +12,10 @@ cd cc_imapsync/
 mv * ../
 chown -R root:root /usr/local/cpanel/base/frontend/paper_lantern/cc_imapsync/
 chmod 755 /usr/local/cpanel/base/frontend/paper_lantern/cc_imapsync/
-/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/cc_imapsync/cc_imapsync.tar.gz
+/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/cc_imapsync/cc_imapsync.tar.gz --theme paper_lantern
 echo "Paper Lantern Installation is complete!"
 
-echo "Installing IMAP Sync UI - Jupiter"
+echo "Installing cPanelIMAP IMAPSYNC GUI for cPanel - Jupiter"
 mkdir -p /usr/local/cpanel/base/frontend/jupiter/cc_imapsync
 cd /usr/local/cpanel/base/frontend/jupiter/cc_imapsync
 wget https://github.com/ccwpmigration/cPanelIMAP/raw/main/cc_imapsync.tar.gz
